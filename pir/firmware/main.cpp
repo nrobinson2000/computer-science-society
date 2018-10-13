efb191fd91ebe0b5c89af8d49d598a269b789fe6
Nathan Robinson <nrobinson2000@me.com>
2018-10-13 00:27:39 -0400

    at 00:27 of 2018-13-10

--- pir/firmware/main.cpp
+++ pir/firmware/main.cpp
@@ -1,11 +1,51 @@
 #include "Particle.h"
 
-void setup() // Put setup code here to run once
-{
+SYSTEM_MODE(AUTOMATIC);
 
+int ledPin = D7;                // choose the pin for the LED
+int inputPin = D0;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+
+unsigned long lastPublish;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+
+  pinMode(A5, OUTPUT);
+
+  Serial.begin(115200);
+
+  lastPublish = millis();
 }
 
-void loop() // Put code here to loop forever
-{
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  if (val == HIGH) {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+    if (pirState == LOW) {
+      // we have just turned on
+      Serial.println("Motion detected!");
+      // We only want to print on the output change, not state
+      pirState = HIGH;
 
+      if (millis() - lastPublish >= 15000)
+      {
+        digitalWrite(A5, HIGH);
+        Particle.publish("motion_detected", PRIVATE);
+        lastPublish = millis();
+      }
+
+    }
+  } else {
+    digitalWrite(A5, LOW);
+    digitalWrite(ledPin, LOW); // turn LED OFF
+    if (pirState == HIGH){
+      // we have just turned of
+      Serial.println("Motion ended!");
+      // We only want to print on the output change, not state
+      pirState = LOW;
+    }
+  }
 }
